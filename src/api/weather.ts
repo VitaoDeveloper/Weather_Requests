98377e5f72fd44a6113fb4cededabd33a46c94a5
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-06-15 15:10:10 -0300

    chore: remove unecessary URL variables

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -3,15 +3,11 @@ import { client } from './client';
 const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
 export async function fetchByCoords(lat: number, lon: number) {
-    const endpoint  = `/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=pt_br`;
-
-    const response = await client.get(endpoint);
+    const response = await client.get(`/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=pt_br`);
     return response.data;
 }
 
 export async function fetchByCity(cityName: string) {
-    const url = `/weather?q=${encodeURIComponent(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`;
-
-    const response = await client.get(url);
+    const response = await client.get(`/weather?q=${encodeURIComponent(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`);
     return response.data;
 }
